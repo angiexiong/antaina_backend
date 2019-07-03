a5ae41b61b42983a923fece9e27725436f59c33e
xaq <3895965156@qq.com>
2019-07-03 14:29:43 +0800

    【仓储】- 出货的同时扣减对应的库存
    Signed-off-by: xaq <3895965156@qq.com>

---
+++ src/main/java/com/antaina/enums/common/StorageOutputTypeEnum.java
@@ -0,0 +1,23 @@
+package com.antaina.enums.common;
+
+/**
+ * @author: xaq
+ * @create: 2019/7/3 14:23
+ * @description: 出库类型
+ */
+public enum StorageOutputTypeEnum {
+    /**
+     * 0:生产出库
+     */
+    PRODUCTION,
+
+    /**
+     * 1:销售出库
+     */
+    SELLING,
+
+    /**
+     * 2:退货出库
+     */
+    REFUND
+}
